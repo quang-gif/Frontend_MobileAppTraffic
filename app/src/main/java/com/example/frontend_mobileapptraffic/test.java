7275514b342d2e7f3c0b332f43998ef349b634ae
Nguyen Viet Quang <quangsts123@outlook.com>
2025-09-26 22:47:12 +0700

    Frontend_MobileAppTraffic

--- app/src/main/java/com/example/frontend_mobileapptraffic/test.java
+++ app/src/main/java/com/example/frontend_mobileapptraffic/test.java
@@ -1,14 +1,29 @@
 package com.example.frontend_mobileapptraffic;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class test extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private MaterialToolbar toolbar;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
+        setContentView(R.layout.login);
+
     }
 }
