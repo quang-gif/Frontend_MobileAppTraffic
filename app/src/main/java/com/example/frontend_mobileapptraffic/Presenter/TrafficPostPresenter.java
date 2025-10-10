2b8f1610a564ec32522be8b6ada3124398ea5248
Le Cong <lechicong9a2@gmail.com>
2025-10-10 21:48:43 +0700

    update chatbot fe

--- app/src/main/java/com/example/frontend_mobileapptraffic/Presenter/TrafficPostPresenter.java
+++ app/src/main/java/com/example/frontend_mobileapptraffic/Presenter/TrafficPostPresenter.java
@@ -85,7 +85,6 @@ public class TrafficPostPresenter {
 
         api = retrofit.create(Api.class);
     }
-
     // --------- Load posts ---------
     public void loadPosts() {
         String token = getToken();
